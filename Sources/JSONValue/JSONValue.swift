4d2962bf08a8cf2fc789f57e38af7597a9fea17c
Edon <edon@valdman.works>
2023-03-07 12:52:47 -0500

    Update JSONValue.swift
    Started adding documentation.

--- Sources/JSONValue/JSONValue.swift
+++ Sources/JSONValue/JSONValue.swift
@@ -7,15 +7,32 @@
 
 import Foundation
 
+/// A container for untyped JSON values.
 @dynamicMemberLookup
 public enum JSONValue: Hashable {
+    /// A JSON string value.
     case string(String)
+    
+    /// A JSON number value.
     case number(Double)
+    
+    /// A JSON boolean value.
     case bool(Bool)
+    
+    /// A JSON object.
+    ///
+    /// All keys are string values, and all values are recursively other `JSONValue`s.
     case object([String: JSONValue])
+    
+    /// A JSON array.
+    ///
+    /// All elemenets of the array are recursively other `JSONValue`s.
     case array([JSONValue])
+    
+    /// A JSON null value.
     case null
     
+    /// A an empty JSON object for convenience.
     public static let emptyObject = JSONValue.object([:])
     
     public subscript(dynamicMember member: String) -> JSONValue? {
